deec701b5dbbc83acee00da07efb505183d083ad
BlamBromme <47858495+Shroker@users.noreply.github.com>
2019-02-21 15:13:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function yes(){
+	window.location.replace("Intro.html");
+}
+
+function screen2(){
+	window.location.replace("Intro2.html")
+}
+
+function screen3(){
+	window.location.replace("Intro3.html")
+}
+
+function ExtraScreen(){
+	window.location.replace("ExtraScreen.html")
+}
+function screen4(){
+	window.location.replace("MiningTime.html")
+}
+function End(){
+	window.location.replace("GameOver.html")
+}
